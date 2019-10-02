7a7b9c8de00731110fe0e9178927c4ec0436148f
Kris Turek <15099890+kristurek@users.noreply.github.com>
2019-10-02 09:52:31 -0700

    LeetCode[7,9,11,12,13]

--- src/main/java/com/kristurek/leetcode/Solution.java
+++ src/main/java/com/kristurek/leetcode/Solution.java
@@ -1,7 +1,9 @@
 package com.kristurek.leetcode;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import com.kristurek.leetcode.common.ListNode;
 
@@ -92,5 +94,137 @@ public class Solution {
 			return nums[nums.length / 2];
 		else
 			return ((double) (nums[(nums.length / 2) - 1] + nums[nums.length / 2])) / 2.0;
+	}
+
+	public int _7_reverse(int x) {
+		boolean minus = false;
+		if (x < 0) {
+			minus = true;
+			x = x * -1;
+		}
+
+		long reverseNumber = 0;
+		long number = x;
+
+		while (number > 0) {
+			reverseNumber = reverseNumber * 10 + number % 10;
+			number = number / 10;
+		}
+
+		if (reverseNumber > Integer.MAX_VALUE || reverseNumber < Integer.MIN_VALUE)
+			return 0;
+
+		if (minus)
+			return (int) reverseNumber * -1;
+		else
+			return (int) reverseNumber;
+	}
+
+	public boolean _9_isPalindrome(int x) {
+		if (x < 0)
+			return false;
+		if (x == 0)
+			return true;
+
+		long reverseNumber = 0;
+		long number = x;
+
+		while (number > 0) {
+			reverseNumber = reverseNumber * 10 + number % 10;
+			number = number / 10;
+		}
+
+		if (reverseNumber > Integer.MAX_VALUE || reverseNumber < Integer.MIN_VALUE)
+			return false;
+
+		return reverseNumber == x;
+	}
+
+	public int _11_maxArea(int[] height) {
+		int max = 0;
+		int l = 0, r = height.length - 1;
+
+		while (l < r) {
+			int area = (r - l) * Math.min(height[l], height[r]);
+			if (area > max)
+				max = area;
+
+			if (height[l] <= height[r])
+				l += 1;
+			else
+				r -= 1;
+		}
+
+		return max;
+	}
+
+	public String _12_intToRoman(int num) {
+		Map<Integer, String> map = new LinkedHashMap<>();
+		map.put(1000, "M");
+		map.put(900, "CM");
+		map.put(500, "D");
+		map.put(400, "CD");
+		map.put(100, "C");
+		map.put(90, "XC");
+		map.put(50, "L");
+		map.put(40, "XL");
+		map.put(10, "X");
+		map.put(9, "IX");
+		map.put(5, "V");
+		map.put(4, "IV");
+		map.put(1, "I");
+
+		StringBuilder roman = new StringBuilder();
+
+		for (Entry<Integer, String> entry : map.entrySet()) {
+			int mult = entry.getKey();
+			String code = entry.getValue();
+
+			for (int i = 0; i < num / mult; i++)
+				roman.append(code);
+
+			num = num % mult;
+		}
+
+		return roman.toString();
+	}
+
+	public int _13_romanToInt(String s) {
+		if (s == null || s.isBlank())
+			throw new IllegalArgumentException("Wrong input param");
+
+		int sum = 0;
+		for (int i = 0; i < s.length() - 1; i++) {
+			int currentChar = _13_romanToInt_convert(s.charAt(i));
+			int nextChar = _13_romanToInt_convert(s.charAt(i + 1));
+
+			if (currentChar >= nextChar)
+				sum += currentChar;
+			else
+				sum -= currentChar;
+		}
+
+		return sum + _13_romanToInt_convert(s.charAt(s.length() - 1));
+	}
+
+	private int _13_romanToInt_convert(Character c) {
+		switch (c) {
+		case 'I':
+			return 1;
+		case 'V':
+			return 5;
+		case 'X':
+			return 10;
+		case 'L':
+			return 50;
+		case 'C':
+			return 100;
+		case 'D':
+			return 500;
+		case 'M':
+			return 1000;
+		default:
+			throw new IllegalArgumentException("Unsupported char");
+		}
 	}
 }
--- src/test/java/com/kristurek/leetcode/SolutionTest.java
+++ src/test/java/com/kristurek/leetcode/SolutionTest.java
@@ -1,5 +1,7 @@
 package com.kristurek.leetcode;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,7 +19,7 @@ public class SolutionTest {
 	}
 
 	@Test
-	void _2_addTwoNumbers() {  
+	void _2_addTwoNumbers() {
 		ListNode l1 = new ListNode(2);
 		l1.next = new ListNode(4);
 		l1.next.next = new ListNode(3);
@@ -45,5 +47,42 @@ public class SolutionTest {
 		assertEquals(2.0, solution._4_findMedianSortedArrays(new int[] { 1, 3 }, new int[] { 2 }));
 		assertEquals(2.5, solution._4_findMedianSortedArrays(new int[] { 1, 2 }, new int[] { 3, 4 }));
 		assertEquals(100000.5, solution._4_findMedianSortedArrays(new int[] { 100000 }, new int[] { 100001 }));
+	}
+
+	@Test
+	void _7_reverse() {
+		assertEquals(321, solution._7_reverse(123));
+		assertEquals(-321, solution._7_reverse(-123));
+		assertEquals(21, solution._7_reverse(120));
+	}
+
+	@Test
+	void _9_isPalindrome() {
+		assertTrue(solution._9_isPalindrome(121));
+		assertFalse(solution._9_isPalindrome(-123));
+		assertFalse(solution._9_isPalindrome(10));
+	}
+
+	@Test
+	void _11_maxArea() {
+		assertEquals(49, solution._11_maxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }));
+	}
+
+	@Test
+	void _12_intToRoman() {
+		assertEquals("III", solution._12_intToRoman(3));
+		assertEquals("IV", solution._12_intToRoman(4));
+		assertEquals("IX", solution._12_intToRoman(9));
+		assertEquals("LVIII", solution._12_intToRoman(58));
+		assertEquals("MCMXCIV", solution._12_intToRoman(1994));
+	}
+
+	@Test
+	void _13_romanToInt() {
+		assertEquals(3, solution._13_romanToInt("III"));
+		assertEquals(4, solution._13_romanToInt("IV"));
+		assertEquals(9, solution._13_romanToInt("IX"));
+		assertEquals(58, solution._13_romanToInt("LVIII"));
+		assertEquals(1994, solution._13_romanToInt("MCMXCIV"));
 	}
 }
